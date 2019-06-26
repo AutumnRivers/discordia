7fbfe9b681f05ca69bb37144cbd85a1e8595f12c
Nati <26543607+SmartieCodes@users.noreply.github.com>
2019-06-25 23:13:07 -0400

    Upgraded build command
    Doesn't do much still. But it now makes users work and detects when they're working. So that's fun.

--- commands/city_build.js
+++ commands/city_build.js
@@ -11,16 +11,48 @@ module.exports = {
         .then(city => {
             var contracts = JSON.parse(city.contracts);
 
-            var contract = contracts.filter(c => (c.id === args[0]));
+            var contractArray = contracts.filter(c => (c.id === args[0]));
 
-            if(!contract[0]) return message.reply("that contract doesn't exist, or you haven't unlocked it yet. View your current contracts with `city!contracts`");
+            if(!contractArray[0]) return message.reply("that contract doesn't exist, or you haven't unlocked it yet. View your current contracts with `city!contracts`");
 
-            var contract = contract[0];
+            var contract = contractArray[0];
 
-            message.channel.send('', {embed: {
-                title: "Construction START!",
-                description: "**<@" + message.author.id + ">** is now working on `" + contract.title + "`."
-            }})
-        })
+	    sqlite.get('SELECT * FROM projects WHERE guildId = ?', [message.guild.id])
+	    .then(guild => {
+		if(!guild) {
+			const contractString = JSON.stringify(contractArray[0]);
+			sqlite.get('SELECT users FROM guilds WHERE id = ?', [message.guild.id])
+			.then(guild => {
+				var users = JSON.parse(guild.users);
+				var cuUser = users.filter(u => (u.id === message.author.id));
+				console.log(cuUser);
+				var userString = JSON.stringify(cuUser);
+				sqlite.run('INSERT INTO projects (guildId, details, workers) VALUES (?, ?, ?)', [message.guild.id, contractString, userString]);
+
+				message.channel.send('', {embed: {
+					title: "Construction START!",
+					description: "**<@" + message.author.id + ">** is now working on `" + contract.title + "`."
+				}})
+			});
+		} else {
+			const contractDetails = JSON.parse(guild.details);
+			const workers = JSON.parse(guild.workers);
+			var userIsWorking = workers.filter(w => (w.id === message.author.id));
+			if(contractDetails.id !== args) return message.reply('You may only work on the current project, `' + contractDetails.title + '`.');
+			if(userIsWorking) return message.reply('You are already working on the project!');
+
+			sqlite.get('SELECT users FROM guilds WHERE id = ?', [message.guild.id])
+			.then(guild => {
+				var users = JSON.parse(guild.users);
+				var cuUser = users.filter(u => (u.id === message.author.id));
+				var userString = JSON.parse(cuUser[0]);
+
+				workers.push(userString);
+				const workerString = JSON.stringify(workers);
+				sqlite.run('UPDATE projects SET workers = ? WHERE guildId = ?', [workerString, message.guild.id]);
+			});
+		}
+	    });
+        });
     }
 }
